7c23c135539cbd9f7a85cacea50beb482cd027d7
XavierCHN <364671459@qq.com>
2014-08-14 07:57:29 +0800

    Init

---
+++ scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,325 @@
+TIME_HERO_SELECTION , TIME_PRE_GAME , TIME_POST_GAME = 10.0 , 10.0 , 10.0
+
+TIME_ROUND_REST = 10.0
+TOTAL_ROUNDS = 11
+
+GOLD_TICK_TIME = 60.0
+GOLD_PER_TICK = 0
+
+
+CUSTOM_STATE_PREPARE = 0
+CUSTOM_STATE_IN_ROUND = 1
+CUSTOM_STATE_ROUND_REST = 2
+CUSTOM_STATE_GAME_END = 3
+
+if SFWarsMode == nil then
+	SFWarsMode = class({})
+end
+
+function Activate()
+	SFWarsMode:InitGameMode()
+end
+
+function SFWarsMode:InitGameMode()
+	self.eGameEntity = GameRules:GetGameModeEntity()
+
+	self:InitPara()
+	self:SetGameRules()
+	self:RegisterEventListener()
+	self:StartThink()
+end
+
+function SFWarsMode:InitPara()
+	self.nCurrState = CUSTOM_STATE_PREPARE
+	self.RoundCount = 0
+	self.nGoodWinRounds = 0
+	self.nBadWinRounds  = 0
+	self._hideHud = {}
+end
+
+function SFWarsMode:SetGameRules()
+	GameRules:SetTimeOfDay( 0.75 )
+	GameRules:SetHeroRespawnEnabled( false )
+	GameRules:SetUseUniversalShopMode( false )
+	GameRules:SetHeroSelectionTime( TIME_HERO_SELECTION )
+	GameRules:SetPreGameTime( TIME_PRE_GAME )
+	GameRules:SetPostGameTime( TIME_POST_GAME )
+	GameRules:SetTreeRegrowTime( 60.0 )
+	--GameRules:SetHeroMinimapIconSize( 400 )
+	--GameRules:SetCreepMinimapIconScale( 0.7 )
+	--GameRules:SetRuneMinimapIconScale( 0.7 )
+	GameRules:SetGoldTickTime( GOLD_TICK_TIME )
+	GameRules:SetGoldPerTick( GOLD_PER_TICK )
+	--GameRules:GetGameModeEntity():SetRemoveIllusionsOnDeath( true )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesOverride( true )
+	--GameRules:GetGameModeEntity():SetTopBarTeamValuesVisible( false )
+end
+
+function SFWarsMode:RegisterEventListener()
+	ListenToGameEvent( "entity_killed", Dynamic_Wrap( SFWarsMode, "OnEntityKilled" ), self )
+	ListenToGameEvent('npc_spawned',Dynamic_Wrap(SFWarsMode,"OnNPCSpawned"),self)
+
+	--ListenToGameEvent('sf_wars_increase_raze_damage0',Dynamic_Wrap(SFWarsMode,"OnInCreaseRazeDamagez"),self)
+	--ListenToGameEvent('sf_wars_increase_raze_damage',Dynamic_Wrap(SFWarsMode,"OnInCreaseRazeDamage"),self)
+end
+
+function SFWarsMode:OnInCreaseRazeDamage(keys)
+	print('HUD CALL INCREASE RAZE DAMAGE SUCCESS')
+end
+function SFWarsMode:OnInCreaseRazeDamagez(keys)
+	print('HUD CALL INCREASE RAZE DAMAGE SUCCESS zzzzzzzzzzzzzz')
+end
+
+function SFWarsMode:StartThink()
+	self.eGameEntity:SetThink('OnThink' , self , 1 )
+end
+
+function SFWarsMode:OnThink()
+	if self.t0 == nil then
+		self.t0 = GameRules:GetGameTime()
+	end
+	local time = GameRules:GetGameTime()
+	local dt = time - self.t0
+	self.t0 = time
+	if GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then return nil else
+
+		if self.nCurrState == CUSTOM_STATE_PREPARE then
+			self:ThinkPrePare(dt)		
+		elseif self.nCurrState == CUSTOM_STATE_IN_ROUND then
+			self:ThinkInRound(dt)	
+		elseif self.nCurrState == CUSTOM_STATE_ROUND_REST then
+			self:ThinkRoundRest(dt)
+		elseif self.nCurrState == CUSTOM_STATE_GAME_END then
+			self:ThinkGameEnd()
+		end
+		return 1
+	end
+end
+
+function SFWarsMode:ThinkPrePare(dt)
+	if GameRules:State_Get() > DOTA_GAMERULES_STATE_PRE_GAME then
+		
+		self.nCurrState = CUSTOM_STATE_ROUND_REST
+		
+		local center_msg = {
+			message = 'sfwars_game_start_in_10',
+			duration = 5
+		}
+		FireGameEvent('show_center_message',center_message)
+	end
+end
+
+function SFWarsMode:ThinkRoundRest(dt)
+	if self.fTimeRested == nil then
+		self.fTimeRested = 0
+	end
+	self.fTimeRested = self.fTimeRested + dt
+	if self.fTimeRested >= TIME_ROUND_REST then
+		self.RoundCount = self.RoundCount + 1
+		self.nCurrState = CUSTOM_STATE_IN_ROUND
+		self.fTimeRested = nil
+	else
+		local fTimeLeft = TIME_ROUND_REST - self.fTimeRested
+		if fTimeLeft < 1 then
+			self:ShowCenterMessage('1',0.9)
+			do return end
+		elseif fTimeLeft < 2 then
+			self:ShowCenterMessage('2',0.9)
+			do return end
+		elseif fTimeLeft < 3 then
+			self:ShowCenterMessage('3',0.9)
+			do return end
+		end
+	end
+end
+
+function SFWarsMode:ThinkInRound(dt)
+	local nGoodAlive = 0
+	local nBadAlive = 0
+	for i = 0,9 do
+		local hPlayer = PlayerResource:GetPlayer(i)
+		if hPlayer then
+			local hHero = hPlayer:GetAssignedHero()
+			if hHero then
+				if hHero:IsAlive() then
+					if hPlayer:GetTeam() == DOTA_TEAM_GOODGUYS then
+						nGoodAlive = nGoodAlive + 1
+					elseif hPlayer:GEtTeam() == DOTA_TEAM_BADGUYS then
+						nBadAlive = nBadAlive  + 1
+					end
+				end
+			end
+		end
+	end
+	if nGoodAlive == 0 then
+		self.nBadWinRounds = self.nBadWinRounds + 1
+		self.eGameEntity:SetTopBarTeamValue(DOTA_TEAM_GOODGUYS,self.nGoodWinRounds)
+		self.eGameEntity:SetTopBarTeamValue(DOTA_TEAM_BADGUYS,self.nBadWinRounds)
+	end
+	if nBadAlive == 0 then
+		self.nGoodWinRounds = self.nGoodWinRounds + 1
+		self.eGameEntity:SetTopBarTeamValue(DOTA_TEAM_GOODGUYS,self.nGoodWinRounds)
+		self.eGameEntity:SetTopBarTeamValue(DOTA_TEAM_BADGUYS,self.nBadWinRounds)
+	end
+	local bIsRoundEnd = false
+	if nGoodAlive == 0 or nBadAlive == 0 then
+		bIsRoundEnd = true
+	end
+	if bIsRoundEnd then
+		if self.RoundCount > TOTAL_ROUNDS then
+			self.nCurrState = CUSTOM_STATE_GAME_END
+		else
+			self.nCurrState = CUSTOM_STATE_ROUND_REST
+		end
+	end
+end
+
+function SFWarsMode:IsRoundEnd(goodalive,badalive)
+	if goodalive == 0 then
+		self.nBadWinRounds = self.nBadWinRounds + 1
+		self.eGameEntity:SetTopBarTeamValue(DOTA_TEAM_GOODGUYS,self.nGoodWinRounds)
+		self.eGameEntity:SetTopBarTeamValue(DOTA_TEAM_BADGUYS,self.nBadWinRounds)
+	end
+	if badalive == 0 then
+		self.nGoodWinRounds = self.nGoodWinRounds + 1
+		self.eGameEntity:SetTopBarTeamValue(DOTA_TEAM_GOODGUYS,self.nGoodWinRounds)
+		self.eGameEntity:SetTopBarTeamValue(DOTA_TEAM_BADGUYS,self.nBadWinRounds)
+	end
+	if goodalive == 0 or goodalive == 0 then
+		return true
+	end
+	return false
+end
+
+function SFWarsMode:ThinkGameEnd()
+
+	local WINNER = nil
+	if self.nGoodWinRounds > self.nBadWinRounds then
+		WINNER = DOTA_TEAM_GOODGUYS
+	else
+		WINNER = DOTA_TEAM_BADGUYS
+	end
+
+	GameRules:SetGameWinner(DOTA_TEAM_BADGUYS)
+
+	GameRules:SetSafeToLeave(true)
+end
+
+function SFWarsMode:SetAbilityPoints(hero)
+	for i = 1,hero:GetAbilityCount() do
+		local ABILITY = hero:GetAbilityByIndex(i)
+		if ABILITY then ABILITY:SetLevel(1) end
+	end
+	hero:SetAbilityPoints(0)
+end
+
+
+function SFWarsMode:ShowCenterMessage(msg,dt)
+	if msg and dt then
+		local center_message = {
+			message = msg,
+			duration = dt
+		}
+		FireGameEvent('show_center_message',center_message)
+	end
+end
+
+function SFWarsMode:OnNPCSpawned(keys)
+	print('FUNCTION NPC SPAWN CALLED')
+	local entity = EntIndexToHScript(keys.entindex)
+	if entity then print(entity:GetUnitName()) end
+	if entity:GetUnitName() == 'npc_dota_hero_nevermore' then
+		for i = 0,4 do
+			local ability = entity:GetAbilityByIndex(i)
+			if ability then ability:SetLevel(1) end
+		end
+		entity:SetAbilityPoints(0)
+	end
+
+	local player = entity:GetOwner()
+	if not self._hideHud[player] then
+		self:HideHud()
+		self._hideHud[player] = true
+	end
+end
+
+function SFWarsMode:HideHud()
+	print('function hide hud called')
+	SendToServerConsole('sv_cheats 1')
+	SendToConsole('dota_sf_hud_actionpanel 0')--去除技能动作条
+	SendToConsole('dota_sf_hud_inventory 0')--去除物品栏
+	SendToConsole('dota_sf_hud_top 0')--去除上边栏
+	SendToConsole('dota_hud_healthbars 0 ')--去除血条
+	SendToConsole('dota_no_minimap 1')--去除小地图
+	SendToConsole('dota_sf_hud_stats_dropdown 0')--去除左上角
+	SendToConsole('cl_drawhud 0')
+	SendToConsole('dota_sf_hud_overlay 0')
+	SendToConsole('dota_render_crop_height 0')
+	SendToConsole('dota_render_y_inset 0')
+	SendToConsole('dota_free_camera 1')
+	SendToServerConsole('sv_cheats 0')
+end
+
+-- ABILITY HOOKS
+function OnRaze(keys)
+	for k,v in pairs(keys) do
+		print(k..'[]'..tostring(v))
+	end
+	local caster = keys.caster or EntIndexToHScript(keys.caster_entindex)
+	local ABILITY = keys.ability
+
+	local casterOrigin = caster:GetOrigin()
+	local casterFV = caster:GetForwardVector()
+
+	if caster:GetContext('raze_damage') == nil then
+		caster:SetContext('raze_damage','250',0)
+	end
+	if caster:GetContext('bonus_radius') == nil then
+		caster:SetContext('bonus_radius','0',0)
+	end
+	local nRazeDamage = caster:GetContext('raze_damage')
+	local nRazeRadius = caster:GetContext('bonus_radius') + 250
+	local nRazeRange = 0
+	if ABILITY:GetName() == 'sfwars_shadowraze_1' then
+		nRazeRange = 200
+	elseif ABILITY:GetName() == 'sfwars_shadowraze_2' then
+		nRazeRange = 450
+	elseif ABILITY:GetName() == 'sfwars_shadowraze_3' then
+		nRazeRange = 700
+	end
+
+	local vCenter = casterOrigin + (casterFV * nRazeRange)
+	print(tostring(vCenter))
+
+	local nPIndex = ParticleManager:CreateParticle('particles/units/heroes/hero_nevermore/nevermore_shadowraze.vpcf',
+		PATTACH_CUSTOMORIGIN,caster)
+	ParticleManager:SetParticleControl(nPIndex,0,vCenter)
+	ParticleManager:ReleaseParticleIndex(nPIndex)
+	
+	local targets = FindUnitsInRadius(
+		caster:GetTeam(),       --caster team
+        vCenter,       --find position
+        nil,                    --find entity
+        nRazeRadius,                    --find radius
+        DOTA_UNIT_TARGET_TEAM_ENEMY,
+        DOTA_UNIT_TARGET_ALL,
+        0, FIND_CLOSEST,
+        false
+	)
+	if targets then
+		print('target found  '..#targets)
+		for k,v in pairs(targets) do
+			local damage_dealt = ApplyDamage({
+				victim = v,
+				attacker = caster,
+				damage = nRazeDamage,
+				damage_type = DAMAGE_TYPE_MAGICAL,
+				damage_flags = 0,
+				ability = ABILITY
+			})
+			if damage_dealt then
+				print('damage_deal .. '..damage_dealt)
+			end
+		end
+	end
+end
